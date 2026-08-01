3ff7d3233eb0a7d8bc3cb8dc5a31aac719bb4e23
ABHINAV REDDY PUTTA <abhinav.putta@fonemed.com>
2026-08-01 00:26:29 -0230

    add basic REPL with .exit

--- src/main.zig
+++ src/main.zig
@@ -1,71 +1,40 @@
 const std = @import("std");
-const Io = std.Io;
 
 const puffinDB = @import("puffinDB");
 
+fn printTerminal(stdout: *std.Io.Writer, comptime text: []const u8, args: anytype) !void {
+    try stdout.print(text, args);
+    try stdout.flush();
+}
+
 pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // This is appropriate for anything that lives as long as the process.
-    const arena: std.mem.Allocator = init.arena.allocator();
-
-    // Accessing command line arguments:
-    const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
-    }
-
-    // In order to do I/O operations need an `Io` instance.
     const io = init.io;
 
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
+    var std_file_writer = std.Io.File.stdout().writer(io, &stdout_buffer);
+    const stdout = &std_file_writer.interface;
 
-    try puffinDB.printAnotherMessage(stdout_writer);
+    var stdin_buffer: [1024]u8 = undefined;
+    var std_file_reader = std.Io.File.stdin().reader(io, &stdin_buffer);
+    const stdin = &std_file_reader.interface;
 
-    try stdout_writer.flush(); // Don't forget to flush!
-}
+    while (true) {
+        try printTerminal(stdout, "db > ", .{});
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+        const input_line = (try stdin.takeDelimiter('\n')) orelse break;
+        var command: []const u8 = "";
 
-test "fuzz example" {
-    try std.testing.fuzz({}, testOne, .{});
-}
+        //strip trailing '\r' if present
+        if (input_line.len > 0 and input_line[input_line.len - 1] == '\r') {
+            command = input_line[0 .. input_line.len - 1];
+        } else {
+            command = input_line;
+        }
 
-fn testOne(context: void, smith: *std.testing.Smith) !void {
-    _ = context;
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(u8) = .empty;
-    defer list.deinit(gpa);
-    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
-        .add_data => {
-            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
-            smith.bytes(slice);
-        },
-        .dup_data => {
-            if (list.items.len == 0) continue;
-            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
-            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
-            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
-            try list.appendSlice(gpa, list.items[off..][0..len]);
-            try std.testing.expectEqualSlices(
-                u8,
-                list.items[off..][0..len],
-                list.items[list.items.len - len ..],
-            );
-        },
-    };
+        if (std.mem.eql(u8, command, ".exit")) {
+            break;
+        } else {
+            try printTerminal(stdout, "Unrecognized command '{s}'. \n", .{command});
+        }
+    }
 }
